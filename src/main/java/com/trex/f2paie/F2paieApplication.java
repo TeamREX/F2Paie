b84eeee9b2cbdddf9488aee0cf781a367c884c97
Yassine <yassine.bt.90@gmail.com>
2018-03-28 22:24:30 +0100

    Initial commit

---
+++ src/main/java/com/trex/f2paie/F2paieApplication.java
@@ -0,0 +1,12 @@
+package com.trex.f2paie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F2paieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F2paieApplication.class, args);
+	}
+}
