721ca521fa5556f67bef84edd4bcfa214ed89fa6
yassine <yassine.bt.90@gmail.com>
2018-03-29 09:56:40 +0100

    From Codix

---
+++ src/main/java/com/trex/f2paie/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.trex.f2paie;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(F2paieApplication.class);
+	}
+
+}
