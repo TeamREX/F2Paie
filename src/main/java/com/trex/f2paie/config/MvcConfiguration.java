6daedf6de4c87d4d8cd9ddd86b4a1ca1178c7947
Yassine <yassine.bt.90@gmail.com>
2018-03-29 23:17:53 +0100

    It WORKs

--- src/main/java/com/trex/f2paie/config/MvcConfiguration.java
+++ src/main/java/com/trex/f2paie/config/MvcConfiguration.java
@@ -1,22 +1,27 @@
 package com.trex.f2paie.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
+@ComponentScan
 public class MvcConfiguration extends WebMvcConfigurerAdapter{
-    @Bean
-    public ViewResolver getViewResolver() {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
-        return resolver;
+        resolver.setViewClass(JstlView.class);
+        registry.viewResolver(resolver);
     }
 
     @Override
